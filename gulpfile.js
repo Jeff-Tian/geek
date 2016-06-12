3647b29db7fa8be9fd3a88ef0172c7f951b4493f
田杰 <jefftian@hcdlearning.com>
2016-06-12 14:07:47 +0800

    Ensuare phantomjs path.

--- gulpfile.js
+++ gulpfile.js
@@ -20,11 +20,21 @@ gulp.task('start', function (done) {
     sh.exec('node app.js', done);
 });
 
-gulp.task('test', function (done) {
+gulp.task('test', ['ensurePhantomJsPath', 'karma']);
+
+gulp.task('karma', function (done) {
     karma.start({
         configFile: __dirname + '/tests/karma.conf.js',
         singleRun: true
     }, done);
+});
+
+gulp.task('ensurePhantomJsPath', function (done) {
+    process.env.PHANTOMJS_BIN = './node_modules/phantomjs-prebuilt/phantomjs';
+
+    console.log(process.env.PHANTOMJS_BIN);
+
+    done();
 });
 
 gulp.task('release', ['jshint', 'mocha', 'test']);
